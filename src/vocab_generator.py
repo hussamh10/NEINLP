5d130ff2c9c00715d13c4609c679d3c59b5959a9
hussamh10 <hussamh10@gmail.com>
2018-03-01 15:33:24 +0500

    vocab generated

---
+++ src/vocab_generator.py
@@ -0,0 +1,20 @@
+file_path = "..\\data\\normalized_whatsapp"
+file = open(file_path, 'r', encoding='utf8')
+data = file.read()
+file.close()
+
+data = data.encode('ascii',"ignore")
+data = data.decode('ascii',"ignore")
+data = str(data)
+
+data = data.split()
+print(data.__len__())
+unique_words = set(data)
+data = ""
+for word in unique_words:
+    data += word + '\n'
+
+file_path = "..\\data\\vocab"
+
+file = open(file_path, 'w')
+file.write(data)
